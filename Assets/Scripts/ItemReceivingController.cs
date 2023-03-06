7bfda33efb216bdcc64bd6cb4b8f74dc0ba83ce6
cwmcgiffert <91047092+rcamwm@users.noreply.github.com>
2023-03-06 10:00:07 -0800

    Bride bug fix

--- Assets/Scripts/ItemReceivingController.cs
+++ Assets/Scripts/ItemReceivingController.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class ItemReceivingController : MonoBehaviour
 {
     public GameObject keyItem;
-    public GameObject ItemReceiever;
+    public GameObject[] ItemReceievers;
     public float keyItemDistance = 5f;
     private BoxCollider2D bc;
 
@@ -22,7 +22,8 @@ public class ItemReceivingController : MonoBehaviour
             {
                 // Play destroy animation
                 Destroy(keyItem);
-                Destroy(ItemReceiever);
+                for (int i = 0; i < ItemReceievers.Length; i++)
+                    Destroy(ItemReceievers[i]);
                 Destroy(gameObject);
             }
         }
