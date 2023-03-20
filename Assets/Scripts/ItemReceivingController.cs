e7f90481f4ca938c16e90bfaeadc7f4a96e54678
cwmcgiffert <91047092+rcamwm@users.noreply.github.com>
2023-03-20 04:19:54 -0700

    Shekhinah bride disappears animation

--- Assets/Scripts/ItemReceivingController.cs
+++ Assets/Scripts/ItemReceivingController.cs
@@ -4,14 +4,39 @@ using UnityEngine;
 
 public class ItemReceivingController : MonoBehaviour
 {
-    public GameObject keyItem;
-    public GameObject[] ItemReceievers;
     public float keyItemDistance = 5f;
+    public GameObject keyItem;
+    public GameObject[] animateReceievers;
+    private Animator[] receiverAnimators;
+    public GameObject[] destroyAfter;
     private BoxCollider2D bc;
 
     void Start()
     {
         bc = GetComponent<BoxCollider2D>();
+        receiverAnimators = new Animator[animateReceievers.Length];
+        for (int i = 0; i < receiverAnimators.Length; i++)
+        {
+            receiverAnimators[i] = animateReceievers[i].GetComponent<Animator>(); 
+        }
+    }
+
+    void Update()
+    {
+        bool allFinished = true;
+        for (int i = 0; i < receiverAnimators.Length; i++)
+        {
+            if (receiverAnimators[i].GetCurrentAnimatorClipInfo(0)[0].clip.name != "Finished")
+                allFinished = false;
+        }
+
+        if (allFinished)
+        { 
+            for (int i = 0; i < destroyAfter.Length; i++)
+            {
+                Destroy(destroyAfter[i]);
+            }
+        }
     }
 
     void OnTriggerEnter2D(Collider2D other)
@@ -20,11 +45,9 @@ public class ItemReceivingController : MonoBehaviour
         {
             if (Vector2.Distance(transform.position, keyItem.transform.position) < keyItemDistance)
             {
-                // Play destroy animation
                 Destroy(keyItem);
-                for (int i = 0; i < ItemReceievers.Length; i++)
-                    Destroy(ItemReceievers[i]);
-                Destroy(gameObject);
+                for (int i = 0; i < animateReceievers.Length; i++)
+                    receiverAnimators[i].SetTrigger("Destroy");
             }
         }
     }
